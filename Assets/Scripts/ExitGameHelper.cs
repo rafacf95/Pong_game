a5038cc73059c5b6a2ec7f0e09d0fdc9debafe0e
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-12 11:40:09 -0300

    Created ExitGameHelper.cs

---
+++ Assets/Scripts/ExitGameHelper.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGameHelper : MonoBehaviour
+{
+     public void Quit()
+    {
+        Application.Quit();
+    }
+}
